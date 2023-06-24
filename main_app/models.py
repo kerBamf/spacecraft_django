014f2b44b57cde907cbb63c870da19f0019c1c1a
kerBamf <zach.pedigo@gmail.com>
2023-06-24 17:35:41 -0400

    Completed through Manufacturer detail page

--- main_app/models.py
+++ main_app/models.py
@@ -6,6 +6,7 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=250)
     about = models.TextField(max_length=500)
+    verified_manufacturer = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
     path('shipwrights', views.ManufacturerList.as_view(), name="manufacturer_list"),
+    path('shipwrights/new/', views.ManufacturerCreate.as_view(), name="manufacturer_create"),
+    path('shipwrights/<int:pk>/', views.ManufacturerDetail.as_view(), name="manufacturer_detail"),
     path('spacecraft', views.ShipList.as_view(), name="ship_list"),
 ]
